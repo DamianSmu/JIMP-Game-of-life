a98bbe8af7bfac894a3516967fc749e031928ec1
jzgagacz <01142603@pw.edu.pl>
2019-03-31 22:50:44 +0200

    Dodanie modułu grid_text_writer, bugfix w grid_reader

---
+++ grid_text_writer.c
@@ -0,0 +1,16 @@
+#include "grid_text_writer.h"
+#include <stdio.h>
+#include <stdlib.h>
+void grid_text_writer_write (char* path,int height, int width, int* cells){
+	FILE *out = fopen(path, "w");
+	if (out==NULL){
+		printf("Błąd: nie udało się otworzyć pliku o ścieżce: %s\n", path);
+		exit(1);
+	}
+	fprintf(out, "%d %d\n",height, width);
+	for (int i=0;i<height;i++){
+		for (int j=0;j<width;j++)
+			fprintf(out,"%d",cells[i*width+j]);
+		fprintf(out,"\n");
+	}
+}
