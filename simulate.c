92c1b342c5d82b9351103c9184139efb2970a999
DamianAcer <smugorzewski.ds@gmail.com>
2019-04-03 11:05:32 +0200

    Makro ALIVE DEAD

--- simulate.c
+++ simulate.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ALIVE 1
+#define DEAD 0
+
 grid_t simulate_next_gen(grid_t grid)
 {
     int h = grid -> height;
@@ -21,38 +24,23 @@ grid_t simulate_next_gen(grid_t grid)
     {
         for (int j = 0; j < w; j++)
         {
-            int state = cells[i * w + j] ? 1 : 0;
+            int state = cells[i * w + j] ? ALIVE : DEAD;
             int neighbours = 0;
 
-            cells[i*w+((w+j-1)%w)] == 1 ? neighbours++ : 0;
-            cells[i*w+((w+j+1)%w)] == 1 ? neighbours++ : 0;
-            cells[((h+i-1)%h)*w+j] == 1 ? neighbours++ : 0;
-            cells[((h+i+1)%h)*w+j] == 1 ? neighbours++ : 0;
-            cells[((h+i-1)%h)*w+((w+j-1)%w)] == 1 ? neighbours++ : 0;
-            cells[((h+i-1)%h)*w+((w+j+1)%w)] == 1 ? neighbours++ : 0;
-            cells[((h+i+1)%h)*w+((w+j-1)%w)] == 1 ? neighbours++ : 0;
-            cells[((h+i+1)%h)*w+((w+j+1)%w)] == 1 ? neighbours++ : 0;
+            cells[i*w+((w+j-1)%w)] == ALIVE ? neighbours++ : 0;
+            cells[i*w+((w+j+1)%w)] == ALIVE ? neighbours++ : 0;
+            cells[((h+i-1)%h)*w+j] == ALIVE ? neighbours++ : 0;
+            cells[((h+i+1)%h)*w+j] == ALIVE ? neighbours++ : 0;
+            cells[((h+i-1)%h)*w+((w+j-1)%w)] == ALIVE ? neighbours++ : 0;
+            cells[((h+i-1)%h)*w+((w+j+1)%w)] == ALIVE ? neighbours++ : 0;
+            cells[((h+i+1)%h)*w+((w+j-1)%w)] == ALIVE ? neighbours++ : 0;
+            cells[((h+i+1)%h)*w+((w+j+1)%w)] == ALIVE ? neighbours++ : 0;
 
-            if(state == 1)
-                new_cells[i*w+j] = ((neighbours == 3 || neighbours == 2) ? 1 : 0);
+            if(state == ALIVE)
+                new_cells[i*w+j] = ((neighbours == 3 || neighbours == 2) ? ALIVE : DEAD);
             else
-                new_cells[i*w+j] = ((neighbours == 3) ? 1 : 0);
-
-
-           // printf("%d , %d  - %d - s: %d\n",i, j, neighbours, state);
+                new_cells[i*w+j] = ((neighbours == 3) ? ALIVE : DEAD);
         }
     }
-
-   /* for (int i = 0; i < h;  i++)
-    {
-        for (int j = 0; j < w; j++)
-        {
-            int state = new_cells[i * w + j];
-            printf("%d", state);
-        }
-        printf("\n");
-    }
-    printf("\n\n");*/
-
     return new_grid;
 }
