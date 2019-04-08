0e2113f3d93a5ba254779d4ca1e62df57455b705
jzgagacz <01142603@pw.edu.pl>
2019-04-08 16:48:33 +0200

    Bugfix grid.c, usprawnienie test_grid

--- grid.c
+++ grid.c
@@ -1,9 +1,12 @@
 #include "grid.h"
 #include <stdlib.h>
+#include <string.h>
 
 grid_t grid_create(int h, int w, int* cells){
 	grid_t grid = malloc(sizeof *grid);
 	grid->height=h;
 	grid->width=w;
 	grid->cells=malloc(h*w*sizeof *cells);
+	memcpy(grid->cells, cells, grid->height*grid->width*sizeof*cells);
+	return grid;
 }
--- test_grid.c
+++ test_grid.c
@@ -4,14 +4,26 @@
 int main (){
 	int h=3;
 	int w=5;
-	int cells[3*5] = {0,1,1,0,0,0,0,0,1,1,1,1,1,1,1};
+	int cells[3*5] = {0,1,1,0,0
+			 ,0,0,0,1,1
+			 ,1,1,1,1,1};
 	grid_t grid = grid_create(h,w,cells);
-	printf("h=%d\n",h);
-	printf("w=%d\n",w);
-	for (int i=0;i<h;i++){
-		for (int j=0;j<w;j++)
-			printf("%d ",cells[i*w+j]);
-		printf("\n");
+	if (grid->height != h){
+		return 1;
+		printf("Test nieudany\n");
 	}
+	if (grid->width != w){
+		return 1;
+		printf("Test nieudany\n");
+	}
+	for (int i=0;i<h;i++){
+		for (int j=0;j<w;j++){
+			if (grid->cells[i*h+j]!=cells[i*h+j]){
+				printf("Test nieudany\n");
+				return 1;
+				}
+		}
+	}
+	printf("Test udany\n");
 	return 0;
 }
